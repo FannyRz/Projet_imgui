dc8f83956b0ffcb0b1641d59fbda95d5dfd86c6c
FannyRz <fanny.rz.celia@gmail.com>
2025-01-14 14:55:59 +0100

    affichage du plateau avec couleur

--- src/App.cpp
+++ src/App.cpp
@@ -6,63 +6,25 @@
 
 void App::draw_board()
 {
-    for (int i{1}; i <= 64; i++)
+    for (int x{0}; x < 8; x++)
     {
-        ImGui::PushID(i);
-        if (ImGui::Button("Vide", ImVec2{50.f, 50.f}))
-            std::cout << "Clicked button" << i << "\n";
-        ImGui::PopID();
-        if (i % 8 != 0)
+        for (int y{0}; y < 8; y++)
         {
-            ImGui::SameLine();
+            if ((x + y) % 2 == 0)
+            {
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{0.f, 0.f, 0.f, 1.f});
+            }
+            else
+            {
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 1.f, 1.f, 1.f});
+            }
+            if (ImGui::Button("Vide", ImVec2{50.f, 50.f}))
+                std::cout << "Clicked button (" << x << "," << y << ") \n";
+            if (y != 7)
+            {
+                ImGui::SameLine();
+            }
+            ImGui::PopStyleColor();
         }
     }
-
-    // ImGui::PushID(1);
-    // if (ImGui::Button("Tour", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 1\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(2);
-    // if (ImGui::Button("Cavalier", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 2\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(3);
-    // if (ImGui::Button("Fou", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 3\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(4);
-    // if (ImGui::Button("Dame", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 4\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(5);
-    // if (ImGui::Button("Roi", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 5\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(6);
-    // if (ImGui::Button("Fou", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 6\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(7);
-    // if (ImGui::Button("Cavalier", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 7\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
-
-    // ImGui::PushID(8);
-    // if (ImGui::Button("Tour", ImVec2{50.f, 50.f}))
-    //     std::cout << "Clicked button 8\n";
-    // ImGui::PopID();
-    // ImGui::SameLine();
 }
