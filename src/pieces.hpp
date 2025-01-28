51cbd735d4585da888fd8da4cae1a95cbeaefd83
Nyxouu <justineperso03@gmail.com>
2025-01-28 10:27:23 +0100

    [DONE] creation des classes des différentes pièces

--- src/pieces.hpp
+++ src/pieces.hpp
@@ -13,10 +13,63 @@ inline std::vector<std::vector<std::string>> position_pieces{
 }; // tableau de positions initiales des pièces.
 
 class Piece {
-    private:
-        std::string label;
-        int color; //0:blanc 1:noire
+protected:
+    std::string m_label;
+    int         m_color; // 0:blanc 1:noire
+    int         m_x; 
+    int         m_y;
 
-    public:
-        void move(); 
+public:
+    Piece(std::string label, int color, int x, int y);
+    ~Piece();
+    void position();
+    void move();
+};
+
+class Tour : public Piece {
+public:
+    Tour(int color, int x, int y)
+        : Piece("Tour", color, x, y)
+    {};
+    ~Tour();
+};
+
+class Cavalier : public Piece {
+public:
+    Cavalier(int color, int x, int y)
+        : Piece("Cavalier", color, x, y)
+    {};
+    ~Cavalier();
+};
+
+class Fou : public Piece {
+public:
+    Fou(int color, int x, int y)
+        : Piece("Fou", color, x, y)
+    {};
+    ~Fou();
+};
+
+class Dame : public Piece {
+public:
+    Dame(int color, int x, int y)
+        : Piece("Dame", color, x, y)
+    {};
+    ~Dame();
+};
+
+class Roi : public Piece {
+public:
+    Roi(int color, int x, int y)
+        : Piece("Roi", color, x, y)
+    {};
+    ~Roi();
+};
+
+class Pion : public Piece {
+public:
+    Pion(int color, int x, int y)
+        : Piece("Pion", color, x, y)
+    {};
+    ~Pion();
 };
