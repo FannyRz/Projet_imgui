a6eca61359d37ddcbbe6846d368863c110b9565f
FannyRz <fanny.rz.celia@gmail.com>
2025-02-11 10:04:04 +0100

    déplacements possibles du roi et du fou -> à vérifier

--- src/pieces.cpp
+++ src/pieces.cpp
@@ -30,6 +30,19 @@ std::vector<std::pair<int, int>> Rook::all_possible_move()
 std::vector<std::pair<int, int>> Bishop::all_possible_move()
 {
     std::vector<std::pair<int, int>> all_theoriq_moves{};
+    for (int x{0}; x < 8; x++)
+    {
+        for (int y{0}; y < 8; y++)
+        {
+            for (int i{1}; i < 7; i++)
+            {
+                if ((this->m_x == x + i && this->m_y == y + i) || (this->m_x != x - i && this->m_y != y - i) || (this->m_x == x + i && this->m_y == y - i) || (this->m_x != x - i && this->m_y != y + i))
+                {
+                    all_theoriq_moves.emplace_back(x, y);
+                }
+            }
+        }
+    }
     return all_theoriq_moves;
 }
 
@@ -42,6 +55,16 @@ std::vector<std::pair<int, int>> Queen::all_possible_move()
 std::vector<std::pair<int, int>> King::all_possible_move()
 {
     std::vector<std::pair<int, int>> all_theoriq_moves{};
+    for (int x{0}; x < 8; x++)
+    {
+        for (int y{0}; y < 8; y++)
+        {
+            if ((this->m_x == x && (this->m_y == y + 1 || this->m_y == y - 1)) || (this->m_y == y && (this->m_x == x + 1 || this->m_x == x - 1)) || ((this->m_y == y + 1 || this->m_y == y - 1) && (this->m_x == x + 1 || this->m_x == x - 1)))
+            {
+                all_theoriq_moves.emplace_back(x, y);
+            }
+        }
+    }
     return all_theoriq_moves;
 }
 
