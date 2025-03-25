d4e33b24f7d020f73b5df87ec0893bc31088a3c4
FannyRz <fanny.rz.celia@gmail.com>
2025-03-25 23:35:42 +0100

    Test 1 -> marche pas

--- src/renderer3D.hpp
+++ src/renderer3D.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include <imgui.h>
+#include "OpenGLutils/object/object.hpp"
 #include "chessBoard.hpp"
+
 
 class Renderer3D {
 private:
+    std::vector<Object> listOfObjects;
+
 public:
     void render_board(ChessBoard& _chessboard);
 };
