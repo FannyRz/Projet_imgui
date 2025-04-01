89ce04aea6ab90ff3eaa63b141634bdf0160009f
Nyxouu <justineperso03@gmail.com>
2025-04-01 14:50:33 +0200

    resolution des pb de merge

--- src/maths/maths_generation_lois.cpp
+++ src/maths/maths_generation_lois.cpp
@@ -96,56 +96,7 @@ double variance_experimentale_exponentielle(std::vector<double> &echantillons, d
   return somme/echantillons.size();
 }
 
-/********************************************************EXPONENTIELLE***************************************************/
 
-// Fonction pour générer un nombre selon la loi exponentielle
-double exponentielle(double lambda, double min, double max) {
-    double U = genererDouble(min, max); // Générer un nombre aléatoire entre 0 et 1
-    // Ajouter une vérification pour éviter que U soit trop proche de 0
-    if (U <= 0.0001) {
-        U = 0.0001; // Fixer U à une valeur minimale acceptable
-        std::cout << "Valeur de U trop faible, ajustée à " << U << std::endl;
-    }
-    return -std::log(U) / lambda;
-}
-
-double variance_experimentale_exponentielle(std::vector<double> &echantillons, double moyenne){
-  double somme = 0.0;
-  for (double echantillon : echantillons){
-    somme += pow(echantillon-moyenne, 2) ;
-  }
-  return somme/echantillons.size();
-}
-
-// Fonction pour générer une variable aléatoire suivant une loi uniforme [0,1]
-float generateUniform()
-{
-    std::random_device                    rd;
-    std::mt19937                          gen(rd());
-    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
-    return dist(gen);
-}
-
-// Fonction pour générer une variable suivant une loi exponentielle de paramètre 1/beta
-float generateExponential(float beta)
-{
-    float U = generateUniform();
-    return -beta * std::log(U);
-}
-
-// Fonction pour générer une variable suivant une loi Gamma
-float generateGamma(int alpha, float beta)
-{
-    float sum = 0.0f;
-
-    // Somme de alpha variables exponentielles
-    for (int i = 0; i < alpha; ++i)
-    {
-        sum += generateExponential(beta);
-    }
-
-    return sum;
-}
 // Fonction pour générer une variable aléatoire suivant une loi uniforme [0,1]
 float generateUniform()
 {
